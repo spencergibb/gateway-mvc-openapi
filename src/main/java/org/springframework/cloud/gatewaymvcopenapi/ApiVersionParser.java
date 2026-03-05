83b5fab653fcb8e5abad2e3b7b04f7aa05abec71
spencergibb <spencer@gibb.us>
2026-03-04 23:49:43 -0500

    Adds support for spring-cloud-loadbalancer and creating openapi ref from discovery

--- src/main/java/org/springframework/cloud/gatewaymvcopenapi/ApiVersionParser.java
+++ src/main/java/org/springframework/cloud/gatewaymvcopenapi/ApiVersionParser.java
@@ -1,24 +1,21 @@
 package org.springframework.cloud.gatewaymvcopenapi;
 
-public class ApiVersionParser implements org.springframework.web.accept.ApiVersionParser {
+import org.springframework.web.accept.SemanticApiVersionParser;
+
+public class ApiVersionParser extends SemanticApiVersionParser {
 
     // allows us to use /api/v2/users instead of /api/2.0/users
     @Override
-    public Comparable parseVersion(String version) {
+    public Version parseVersion(String version) {
         // Remove "v" prefix if it exists (v1 becomes 1, v2 becomes 2)
         if (version.startsWith("v") || version.startsWith("V")) {
             version = version.substring(1);
         }
+
+		if (version.endsWith("+")) {
+			version = version.substring(0, version.length() - 1);
+		}
 		
-		/*if("api-docs".equals(version) || "index.html".equals(version)
-				|| "swagger-ui-bundle.js".equals(version)
-				|| "swagger-ui.css".equals(version)
-				|| "index.css".equals(version)
-				|| "swagger-ui-standalone-preset.js".equals(version)
-				|| "favicon-32x32.png".equals(version)
-				|| "favicon-16x16.png".equals(version)
-				|| "swagger-initializer.js".equals(version)) 
-			return null;*/
-        return version;
+        return super.parseVersion(version);
     }
 }
--- src/main/java/org/springframework/cloud/gatewaymvcopenapi/GatewayMvcOpenapiApplication.java
+++ src/main/java/org/springframework/cloud/gatewaymvcopenapi/GatewayMvcOpenapiApplication.java
@@ -1,44 +1,24 @@
 package org.springframework.cloud.gatewaymvcopenapi;
 
-import java.lang.annotation.Annotation;
-import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
-import io.swagger.v3.core.converter.AnnotatedType;
-import io.swagger.v3.core.converter.ModelConverter;
-import io.swagger.v3.core.converter.ModelConverters;
-import io.swagger.v3.core.converter.ResolvedSchema;
-import io.swagger.v3.oas.annotations.StringToClassMapItem;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.media.Schema;
-import org.springdoc.core.fn.builders.apiresponse.Builder;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.ProxyExchange;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder;
-import static org.springdoc.core.fn.builders.arrayschema.Builder.arraySchemaBuilder;
-import static org.springdoc.core.fn.builders.content.Builder.contentBuilder;
-import static org.springdoc.core.fn.builders.parameter.Builder.parameterBuilder;
-import static org.springdoc.core.fn.builders.schema.Builder.schemaBuilder;
 import static org.springdoc.webmvc.core.fn.SpringdocRouteBuilder.route;
 import static org.springframework.cloud.gateway.server.mvc.filter.AfterFilterFunctions.addResponseHeader;
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.routeId;
-import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
-import static org.springframework.web.servlet.function.RequestPredicates.path;
-//import static org.springframework.web.servlet.function.RequestPredicates.version;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.version;
 
 @SpringBootApplication
@@ -50,53 +30,25 @@ public class GatewayMvcOpenapiApplication {
 	// http :8080/anything/versioned X-API-Version:1.0.0
 	// http :8080/anything/versioned X-API-Version:0.9.0
 	@Bean
-	public RouterFunction<ServerResponse> version20() {
+	public RouterFunction<ServerResponse> version20(LoadBalancerClient lbClient) {
 		// @formatter:off
 		return route()
 				.GET("/anything/versioned", version("2.0+"), http(),
-						ops -> ops.operationId("versioned") //.beanClass(Response.class).beanMethod("anything"))
-								.response(responseBuilder().responseCode("200").ref("http://localhost:8080/httpbin-spec.json#/paths/anything/{anything}/get/responses/200")))
-								// .response(responseBuilder().responseCode("200").implementationArray(UserDTOv1.class)))
-								//.parameter(parameterBuilder().in(ParameterIn.HEADER).name("X-API-Version")
-								//		.schema(schemaBuilder().type("string").defaultValue("1.0").allowableValues(new String[]{"1.0", "2.0"}))))
-								/*.response(responseBuilder().responseCode("200")
-										.content(contentBuilder().array(arraySchemaBuilder().schema(
-												schemaBuilder().name("UserDTOv1").type("object")
-														.properties(new StringToClassMapItem[] {
-																mapItem("headers", Map.class),
-																mapItem("name", String.class),
-																mapItem("email", String.class)
-														}))))))*/
-								// .response(responseBuilder().responseCode("200").description("This is normal response description")))
+						ops -> ops.operationId("versioned").response(responseBuilder().ref(getRef(lbClient))))
 				.before(unary(routeId("versionroute20")))
-				// .before(unary(new HttpbinUriResolver()))
-				.filter(lb("httpbin"))
+				.before(unary(new HttpbinUriResolver()))
+				// .filter(lb("httpbin"))
 				.after(addResponseHeader("X-Version", "2.0+"))
 				.build();
 		// @formatter:on
 	}
 
 	@Bean
-	public RouterFunction<ServerResponse> version10() {
+	public RouterFunction<ServerResponse> version10(LoadBalancerClient lbClient) {
 		// @formatter:off
 		return route()
 				.GET("/anything/versioned", version("1.0+"), http(),
-						ops -> ops.operationId("versioned") //.beanClass(Response.class).beanMethod("anything"))
-								.response(responseBuilder().responseCode("200").ref("http://localhost:8080/httpbin-spec.json#/paths/anything/{anything}/get/responses/200")))
-				// .response(responseBuilder().responseCode("200").implementationArray(UserDTOv2.class)))
-				// 				.parameter(parameterBuilder().in(ParameterIn.HEADER).name("X-API-Version")
-				// 						.schema(schemaBuilder().type("string").defaultValue("1.0").allowableValues(new String[]{"1.0", "2.0"})))
-								/*.response(responseBuilder().responseCode("200")
-										.content(contentBuilder().array(arraySchemaBuilder().schema(
-												schemaBuilder().name("UserDTOv2").type("object")
-														.properties(new StringToClassMapItem[] {
-																mapItem("id", Integer.class),
-																mapItem("firstName", String.class),
-																mapItem("lastName", String.class),
-																mapItem("email", String.class)
-														}))))))*/
-								//.response(responseBuilder().responseCode("200").description("This is normal response description")))
-
+						ops -> ops.operationId("versioned").response(responseBuilder().ref(getRef(lbClient))))
 				.before(unary(routeId("versionroute10")))
 				.before(unary(new HttpbinUriResolver()))
 				.after(addResponseHeader("X-Version", "1.0+"))
@@ -104,55 +56,16 @@ public class GatewayMvcOpenapiApplication {
 		// @formatter:on
 	}
 
-	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI().components(new Components()
-				.addSchemas("UserDTOv1", getSchemaWithDifferentDescription(UserDTOv1.class, "work Address" )));
-	}
-
-	private Schema getSchemaWithDifferentDescription(Class className, String description){
-		Map<String, Schema> read = ModelConverters.getInstance().read(new AnnotatedType(className));
-		return read.values().iterator().next();
-		/*ResolvedSchema resolvedSchema = ModelConverters.getInstance()
-				.resolveAsResolvedSchema(
-						new AnnotatedType(className).resolveAsRef(true));
-		return resolvedSchema.schema.description(description);*/
+	private static String getRef(LoadBalancerClient lbClient) {
+		ServiceInstance httpbin = lbClient.choose("httpbin");
+		Map<String, String> metadata = httpbin.getMetadata();
+		String path = metadata.get("response-ref-path");
+		String ref = UriComponentsBuilder.fromUri(httpbin.getUri()).path(path + "/get/responses").build().toString();
+		return ref;
 	}
 
 	static UnaryOperator<ServerRequest> unary(Function<ServerRequest, ServerRequest> function) {
 		return function::apply;
-	}
-
-	public record UserDTOv1(Integer id, String name, String email) {
-	}
-
-	public record UserDTOv2(Integer id, String firstName, String lastName, String email) {
-	}
-
-	@Component
-	static class Response {
-		Map<String, Object> anything() {
-			return Collections.emptyMap();
-		}
-	}
-
-	private StringToClassMapItem mapItem(String key, Class<?> clazz) {
-		return new StringToClassMapItem() {
-			@Override
-			public Class<? extends Annotation> annotationType() {
-				return StringToClassMapItem.class;
-			}
-
-			@Override
-			public String key() {
-				return key;
-			}
-
-			@Override
-			public Class<?> value() {
-				return clazz;
-			}
-		};
 	}
 
 	public static void main(String[] args) {
