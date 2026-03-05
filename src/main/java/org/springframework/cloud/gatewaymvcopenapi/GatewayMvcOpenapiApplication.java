a917cd766e271285dda754edcefe7ac740cee9cc
spencergibb <spencer@gibb.us>
2026-03-05 01:10:17 -0500

    setup for user api

--- src/main/java/org/springframework/cloud/gatewaymvcopenapi/GatewayMvcOpenapiApplication.java
+++ src/main/java/org/springframework/cloud/gatewaymvcopenapi/GatewayMvcOpenapiApplication.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
@@ -18,41 +19,55 @@ import static org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder
 import static org.springdoc.webmvc.core.fn.SpringdocRouteBuilder.route;
 import static org.springframework.cloud.gateway.server.mvc.filter.AfterFilterFunctions.addResponseHeader;
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.routeId;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.version;
 
 @SpringBootApplication
 public class GatewayMvcOpenapiApplication {
 
+	@Autowired
+	LoadBalancerClient lbClient;
+
 	// http :8080/v3/api-docs
 
-	// http :8080/anything/versioned X-API-Version:2.0.1
-	// http :8080/anything/versioned X-API-Version:1.0.0
-	// http :8080/anything/versioned X-API-Version:0.9.0
+	// http :8080/users X-API-Version:2.0
+	// http :8080/users X-API-Version:1.0
+	// http :8080/users X-API-Version:0.9
 	@Bean
-	public RouterFunction<ServerResponse> version20(LoadBalancerClient lbClient) {
+	public RouterFunction<ServerResponse> gatewayRouterFunction() {
 		// @formatter:off
 		return route()
-				.GET("/anything/versioned", version("2.0+"), http(),
-						ops -> ops.operationId("versioned").response(responseBuilder().ref(getRef(lbClient))))
-				.before(unary(routeId("versionroute20")))
-				.before(unary(new HttpbinUriResolver()))
-				// .filter(lb("httpbin"))
-				.after(addResponseHeader("X-Version", "2.0+"))
-			.GET("/anything/versioned", version("1.0+"), http(),
-					ops -> ops.operationId("versioned").response(responseBuilder().ref(getRef(lbClient))))
-				.before(unary(routeId("versionroute10")))
-				.before(unary(new HttpbinUriResolver()))
-				.after(addResponseHeader("X-Version", "1.0+"))
-				.build();
+			.GET("/userdocs", http(), ops -> ops.operationId("getUserDocs"))
+				.before(unary(routeId("userapidocs")))
+				.before(unary(setPath("/v3/api-docs")))
+				.filter(lb("users"))
+				.build().and(route()
+			.GET("/users", version("2.0"), http(),
+					ops -> ops.operationId("usersv2").response(responseBuilder()
+							.ref(getRef("/paths/~1api~12.0~1users/get/responses"))))
+				.before(unary(routeId("usersv2")))
+				.before(unary(setPath("/api/2.0/users")))
+				.filter(lb("users"))
+				.after(addResponseHeader("X-Version", "2.0"))
+				.build().and(route()
+			.GET("/users", version("1.0"), http(),
+					ops -> ops.operationId("usersv1").response(responseBuilder()
+							.ref(getRef("/paths/~1api~1v1.0~1users/get/responses"))))
+				.before(unary(routeId("usersv1")))
+				.before(unary(setPath("/api/v1.0/users")))
+				.filter(lb("users"))
+				.after(addResponseHeader("X-Version", "1.0"))
+				.build()));
 		// @formatter:on
 	}
 
-	private static String getRef(LoadBalancerClient lbClient) {
-		ServiceInstance httpbin = lbClient.choose("httpbin");
-		Map<String, String> metadata = httpbin.getMetadata();
+	private String getRef(String pathsegment) {
+		ServiceInstance instance = lbClient.choose("users");
+		Map<String, String> metadata = instance.getMetadata();
 		String path = metadata.get("response-ref-path");
-		String ref = UriComponentsBuilder.fromUri(httpbin.getUri()).path(path + "/get/responses").build().toString();
+		String ref = UriComponentsBuilder.fromUriString("http://localhost:8080").path(path + pathsegment).build().toString();
 		return ref;
 	}
 
